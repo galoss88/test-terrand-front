cd38bf3cbb5ddb97320b43b458e7d66f5e85a0b7
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-01 09:28:55 -0300

    Feat: Add app routes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,36 @@
+import { Navigate, useRoutes } from "react-router";
+import ProtectedRoute from "./ProtectedRoute";
+// import Home from "@/pages/home";
+// import { Login } from "@/pages/auth";
+import { lazy } from "react";
+//Aplicamos lazy loading a las rutas de la aplicacion
+const Login = lazy(() => import('@/pages/auth/Login'))
+
+const Home = lazy(() => import("@/pages/home"));
+
+
+//Rutas de la aplicacion
+export const AppRoutes = () => {
+  const routesApp = [
+    {
+      path: "/auth",
+      children: [
+        {
+          path: "login",
+          element: <Login />,
+        },
+      ],
+    },
+    {
+      path: "/",
+      element: <ProtectedRoute />,
+      children: [
+        { index: true, element: <Navigate to="/home" /> },
+        { path: "home", element: <Home /> },
+      ],
+    },
+  ];
+  const routes = useRoutes(routesApp);
+
+  return routes;
+};
