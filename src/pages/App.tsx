cb33e6fa97d2e919b0e12da5163fc229b2df839f
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-01 09:53:10 -0300

    Feat: Add ThemeProvider material-ui and css baseline for normalize global styles

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,13 @@
 import { RoutesWithSuspense } from "@/routes/routes";
+import { theme } from "@/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RoutesWithSuspense />
-    </>
+    </ThemeProvider>
   );
 }
 
