c13055acda3ebc79c26d34b23da599cb39d8230b
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-04 14:52:48 -0300

    Add disableGutters prop to Container in App component for improved layout

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,7 @@ import { Container, CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth={false}>
+      <Container maxWidth={false} disableGutters>
         <CssBaseline />
         <RoutesWithSuspense />
       </Container>
