f2c585e8d129def0bf32f0a0ed63b6661e4b4c94
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-04 15:34:49 -0300

    Add Header component with navigation menu and integrate into App component

---
+++ src/layout/Header.tsx
@@ -0,0 +1,54 @@
+import { StyledContainer, StyledText } from "@/pages/auth/styles";
+import { Home } from "@mui/icons-material";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import { Box, Divider, Stack } from "@mui/material";
+import { useNavigate } from "react-router";
+const menuButtons = [
+  {
+    title: "Inicio",
+    icon: <Home></Home>,
+    to: "/home",
+  },
+  {
+    title: "Mis recetas",
+    icon: <MenuBookIcon></MenuBookIcon>,
+    to: "/myRecipes",
+  },
+];
+
+const Header = () => {
+  const navigate = useNavigate();
+  return (
+    <StyledContainer
+      fixed
+      maxWidth={"xs"}
+      sx={{ maxHeight: { xs: 60, sm: 60 }, mt: .5 }}
+    >
+      <Stack
+        direction="row"
+        divider={<Divider orientation="vertical" flexItem />}
+        spacing={2}
+      >
+        {menuButtons.map((menu) => {
+          return (
+            <Box
+              key={menu.title}
+              onClick={() => navigate(menu.to)} // ← pasa una función, no el resultado
+              sx={{
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+            >
+              {menu.icon}
+              <StyledText>{menu.title}</StyledText>
+            </Box>
+          );
+        })}
+      </Stack>
+    </StyledContainer>
+  );
+};
+
+export default Header;
