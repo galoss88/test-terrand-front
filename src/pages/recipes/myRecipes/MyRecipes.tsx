ebe7e6a44cf1c866de0d1869f491d9b8bacd6dd4
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-05 12:48:19 -0300

    Refactor recipeService to use getAllById method in MyRecipes component for data fetching

--- src/pages/recipes/myRecipes/MyRecipes.tsx
+++ src/pages/recipes/myRecipes/MyRecipes.tsx
@@ -1,20 +1,20 @@
 import { StyledContainer } from "@/pages/auth/styles";
+import { recipeService } from "@/services/recipes/recipesService";
 import { useEffect, useState } from "react";
 import { ListRecipes } from "./components";
 import { IRecipe } from "./types";
 
 const MyRecipes = () => {
   const [recipes, setRecipes] = useState<IRecipe[]>([]);
+  
   useEffect(() => {
     const fetchData = async () => {
-      const url = "http://localhost:3000/recipes";
-      const idUser = 1;
-      const response = await fetch(`${url}?userId=${idUser}`);
-      const data = await response.json();
+      const data = await recipeService.getAllById("1");
       setRecipes(data);
     };
     fetchData();
   }, []);
+
   return (
     <StyledContainer maxWidth={false} sx={{ backgroundColor: "tranparent" }}>
       Mis recetas
--- src/services/recipes/recipesService.ts
+++ src/services/recipes/recipesService.ts
@@ -2,14 +2,23 @@ import { IRecipe } from "@/pages/recipes/myRecipes/types";
 import { fetchApi } from "@/utils/api";
 
 export const recipeService = {
-  getById(id: number): Promise<IRecipe> {
+  getAllById(id: string): Promise<IRecipe[]> {
+    const url = "http://localhost:3000/recipes";
+    const idUser = id;
+
+    return fetchApi<IRecipe[]>({
+      url: `${url}?userId=${idUser}`,
+      method: "GET",
+    });
+  },
+  getById(id: string): Promise<IRecipe> {
     return fetchApi<IRecipe>({
       url: `http://localhost:3000/recipes/${id}`,
       method: "GET",
     });
   },
 
-  update(id: number, body: Omit<IRecipe, "id">): Promise<IRecipe> {
+  update(id: string, body: Omit<IRecipe, "id">): Promise<IRecipe> {
     return fetchApi<IRecipe>({
       url: `http://localhost:3000/recipes/${id}`,
       method: "PUT",
@@ -17,9 +26,9 @@ export const recipeService = {
     });
   },
 
-  create({ body }: { body: Omit<IRecipe, "id"> }) {
+  create({ body }: { body: Omit<IRecipe, "id"> }): Promise<IRecipe> {
     const idUser = "1";
-    fetchApi<IRecipe>({
+    return fetchApi<IRecipe>({
       url: `http://localhost:3000/recipes?id=${idUser}`,
       method: "POST",
       body: body,
