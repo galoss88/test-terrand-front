16278b7249fbdb84b42d33999250f48d8a4d30c7
Nicolas ivan gallo garcia <gallogarcianicolas@gmail.com>
2025-05-07 12:01:02 -0300

    Refactor ImageUploadService: comment out deleteUnusedImage method for future implementation

--- src/services/images/imageUploadService.ts
+++ src/services/images/imageUploadService.ts
@@ -33,22 +33,22 @@ export class ImageUploadService {
       : this.fallbackProvider.getImageUrl(imageId);
   }
 
-  async deleteUnusedImage(imageUrl: string): Promise<boolean> {
-    try {
-      // const provider = imageUrl.includes("cloudinary.com")
-      //   ? this.primaryProvider
-      //   : this.fallbackProvider;
+  // async deleteUnusedImage(imageUrl: string): Promise<boolean> {
+  //   try {
+  //     // const provider = imageUrl.includes("cloudinary.com")
+  //     //   ? this.primaryProvider
+  //     //   : this.fallbackProvider;
 
-      // const urlParts = imageUrl.split("/");
-      // const publicId = urlParts[urlParts.length - 1].split(".")[0];
+  //     // const urlParts = imageUrl.split("/");
+  //     // const publicId = urlParts[urlParts.length - 1].split(".")[0];
 
-      //habilitar delete
+  //     //habilitar delete
 
-      return true;
-    } catch (error) {
-      console.error("Error al eliminar imagen no usada:", error);
-      return false;
-    }
-  }
+  //     return true;
+  //   } catch (error) {
+  //     console.error("Error al eliminar imagen no usada:", error);
+  //     return false;
+  //   }
+  // }
 }
 export const imageUploadService = new ImageUploadService();
